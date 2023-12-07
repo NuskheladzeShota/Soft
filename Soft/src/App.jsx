7c9d9388ed16b45e9b26c998ff6a0c751d9ec739
NuskheladzeShota <nuskheladzeshota@gmail.com>
2023-12-07 15:58:12 +0400

    Your commit message here

---
+++ Soft/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="redBlock">
+          <h1 className='title'>
+              sof
+          </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
